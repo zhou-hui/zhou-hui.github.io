f829725ea14cd487033a9f08135c0d3a5f8d6973
ZhouHui <info@zhouhui.org>
2019-03-29 00:23:28 +0800

    zhou-hui
    tel

---
+++ alipay/index.php
@@ -0,0 +1,130 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<meta id="viewport" name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1; user-scalable=no;" />
+<title>周辉支付宝收款</title>
+<style>
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;vertical-align: baseline;}
+article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {display: block;}
+body {line-height: 1;}
+ol, ul {list-style: none;}
+blockquote, q {quotes: none;}
+blockquote:before, blockquote:after,q:before, q:after {content: '';content: none;}
+table {border-collapse: collapse;border-spacing: 0;}
+
+
+/* Elements */
+
+body { background:#f3f3f3; font-family:Helvetica,"微软雅黑","黑体",Arial,Tahoma;line-height:1.5;text-shadow:0 1px 0px #fff;text-align:center;margin-top:40px;color:#666; }
+
+a:link,a:visited { color:#55a;text-decoration:none; }
+a:hover, a:active { color:#55a;text-decoration:underline; }
+
+/* Elements */
+
+
+.left {float:left}
+.right {float:right}
+.clear {clear:both}
+
+
+.mask { background:#fff;width:100%;height:100%;position:absolute;z-index:998;top:0;left:0;opacity:0.8; }
+
+.dialog { width:400px;border:1px solid #bebebe;position:absolute;z-index:999;box-shadow:0 6px 10px #ccc;border-radius:3px; }
+.dialogContainer { width:100%;border-top:1px solid #fff;background:#333;border-radius:4px;background: -moz-linear-gradient(top, #f0f0f0, #e5e5e5);background: -webkit-gradient(linear, 0 0, 0 100%, from(f0f0f0), to(e5e5e5));}
+.dialogContent { min-height:60px;padding:40px 20px 10px;font-size:20px;color:#666;text-shadow:0 1px 0 #fff;text-align:center;}
+
+
+
+.dialogOperator { padding:10px 20px 20px;text-align:center;margin:0 auto;overflow:hidden; }
+.dialogOperator .inwrap{ float:left; position:relative; left:50%;}
+.dialogOperator .wrapContent { float:left; position:relative; left:-50%; }
+.dialog a.btn { display:block;border-radius:3px;float:left;text-align:center;text-decoration:none;margin:0px 5px 1px 5px; }
+.dialog a.btn b { display:block;height:30px;border-radius:2px;font-size:16px;line-height:30px;text-shadow:none;text-align:center;padding:0 40px;}
+.dialog a.btn:active { margin:1px 5px 0 5px; }
+
+.tourTip .dialogContainer {background:#f0f0f0;background: -moz-linear-gradient(top, #fff, #f9f9f9);background: -webkit-gradient(linear, 0 0, 0 100%, from(fff), to(f9f9f9));}
+.tourTipContent {padding:50px 20px 20px;  }
+.tourTipContent .desc {padding:20px 0;  }
+
+.dialog a.btnClose { position:absolute;right:0;margin:10px;width:24px;height:24px;border-radius:20px;}
+.dialog a.btnClose:hover,.dialog a.btnClose:active { background:#ddd;}
+
+.dialog a.btnClose img { margin:5px 5px 5px 5px; }
+/* Layout */
+
+.container {}
+.clr { clear:both;height:1px;overflow:hidden; }
+.clrLeft { clear:left;height:1px;overflow:hidden; }
+.clrRight { clear:right;height:1px;overflow:hidden; }
+
+
+
+.loginPanel { margin: 10px auto; height:380px; border-radius:4px;text-align:center; }
+
+.loginPanel .title { border-radius:4px 4px 0 0;height:50px;text-shadow:0 1px 0 #fff;font-size:32px;line-height:50px; }
+
+.loginPanel .panelContent { padding:20px; }
+
+.loginPanel .qrcode { width:200px; box-shadow:0 6px 6px #999999; }
+.loginPanel .info { margin:10px 0 0; }
+.loginPanel .normlDesc { }
+.loginPanel .errorMsg { display:none; }
+.loginPanel a.operatorBtn { margin-top:10px; }
+
+
+.zoomIn { cursor:-webkit-zoom-in; cursor: -moz-zoom-in;  }
+.zoomOut { cursor:-webkit-zoom-out; cursor: -moz-zoom-out; }
+
+
+
+
+
+/* Color Scheme */
+
+.slidePic { position:absolute;float:left; }
+.slidePic img { position:absolute;box-shadow:0px 4px 30px #333;z-index:49; }
+
+.normalPanel { }
+
+.lightText { color:#ccc;}
+
+/*tmp style*/ 
+</style>
+</head>
+<body>
+<div id="title">
+</div>
+<div class="main">
+
+<div class="loginPanel normalPanel">
+<div class="title">周辉支付宝收款</div>
+<div class="waiting panelContent">
+<img class="qrcode lightBorder" src="/img/id-alipay.png">
+<div class="info">
+<p class="normlDesc">支付宝账号:Pay@zhouhui.org</p>
+
+<p class="normlDesc">用支付宝客户端扫描二维码以支付</p><p class="errorMsg">成功扫描，请在客户端点击确认以订阅</p>
+</div>
+</div>
+</div>
+
+</div>
+</body>
+</html>
